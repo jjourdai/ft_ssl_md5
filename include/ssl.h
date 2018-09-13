d79570ec42c3fc91cefe766cebd1df02dae31f53
kydiaa <jourdainjulien@gmail.com>
2018-09-13 22:34:05 +0200

    [ADD][UPDATE] add colors.h update ssl.h with define

--- include/ssl.h
+++ include/ssl.h
@@ -17,14 +17,27 @@
 # include "stdint.h"
 # include "stdio.h"
 
-#define LEFT_ROTATE(X, C) ((X << C) | (X >> (32 - C)))
+# define L_ROT(X, C) ((X << C) | (X >> (32 - C)))
+# define R_ROT(X, C) ((X >> C) | (X << (32 - C)))
+# define SWAP_VALUE(X) swap_bigendian_littleendian(X, sizeof(X))
 
-#define RIGHT_ROTATE(X, C) ((X >> C) | (X << (32 - C)))
-#define SWAP_VALUE(X) swap_bigendian_littleendian(X, sizeof(X))
+# define CH(X) ((X[4] & X[5]) ^ ((~X[4]) & X[6]))
+# define MAJ(X) ((X[0] & X[1]) ^ (X[0] & X[2]) ^ (X[1] & X[2]))
+# define S1(X) ((R_ROT(X[4], 6)) ^ (R_ROT(X[4], 11)) ^ (R_ROT(X[4], 25)))
+# define S0(X) ((R_ROT(X[0], 2)) ^ (R_ROT(X[0], 13)) ^ (R_ROT(X[0], 22)))
+
+enum e_macro {
+  MD5,
+  SHA256,
+  ERROR,
+  SUCCESS,
+};
+
+
+
+
+
 //#define INITIAL_VALUE = 0x67452301efcdab8998badcfe10325476;
-
-// uint64_t	origin_string_to_padded_string(char **str);
-
 
 void	md5(char *str);
 void	sha256(char *str);
